4a7ab5ebf4ca34563ceab9375629631f8f917061
nthniareva <nthniareva@gmail.com>
2026-02-24 09:43:36 +0700

    Initial commit

---
+++ application/controllers/hello_world.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class hello_world extends CI_Controller{
+
+    public function index()
+    {
+        echo "Hello world";
+    }
+}
